6be85485f9751745eefc31f4052d05984a8ae99b
Iliass <ielachou@ulb.be>
2022-03-20 20:32:06 +0100

    GTFS static for NY LIRR

---
+++ GTFS-static/NY-LIRR/import.sql
@@ -0,0 +1,23 @@
+COPY agency(agency_id, agency_name, agency_url, agency_timezone, agency_lang, agency_phone)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/agency.txt' DELIMITER ',' CSV HEADER;
+
+COPY calendar_dates(service_id, date, exception_type)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/calendar_dates.txt' DELIMITER ',' CSV HEADER;
+
+COPY feed_info(feed_publisher_name, feed_publisher_url, feed_timezone, feed_lang, feed_version)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/feed_info.txt' DELIMITER ',' CSV HEADER;
+
+COPY routes(route_id, route_short_name, route_long_name, route_type, route_color, route_text_color)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/routes.txt' DELIMITER ',' CSV HEADER;
+
+COPY shapes(shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/shapes.txt' DELIMITER ',' CSV HEADER;
+
+COPY stop_times(trip_id, arrival_time, departure_time, stop_id, stop_sequence)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/stop_times.txt' DELIMITER ',' CSV HEADER;
+
+COPY stops(stop_id, stop_code, stop_name, stop_desc, stop_lat, stop_lon, stop_url, wheelchair_boarding)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/stops.txt' DELIMITER ',' CSV HEADER;
+
+COPY trips(route_id, service_id, trip_id, trip_headsign, trip_short_name, direction_id, shape_id)
+FROM '/mnt/c/Users/HP/Documents/prep master thesis/Preparatory-work/GTFS-static/NY-LIRR/new-york-gtfs/trips.txt' DELIMITER ',' CSV HEADER;
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Preparatory work for master thesis
 
 This repository will store all the necessary code to reproduce the import/export for mobility data exchange standards into MobilityDB.
+
+## GTFS Static
+
+## GTFS Real-time
